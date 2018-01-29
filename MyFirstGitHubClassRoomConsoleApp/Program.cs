418f7b9c41adafee66b2cc2cf157a0f02e6fb273
gdi3000 <dani056i@edu.easj.dk>
2018-01-29 10:38:16 +0100

    Jeg er nice. Jeg har godt hår.

--- MyFirstGitHubClassRoomConsoleApp/Program.cs
+++ MyFirstGitHubClassRoomConsoleApp/Program.cs
@@ -12,6 +12,9 @@ namespace MyFirstGitHubClassRoomConsoleApp
         {
             //Opgave tilføj en Console.writeline med noget tekst og commit + push
             //se herefter på dit GitHub Classroom repo om ændringen er kommet op 
+
+            Console.WriteLine("Jeg har prøvet at skyde med en klassisk MG42, 1200 skud i minuttet, eller 20 skud i sekundet. \n Den slår dog ikke min kæreste der kan fyrer 100 spørgsmål i hovedet på mig så snart hun går gennem døren");
+            Console.ReadKey();
         }
     }
 }
